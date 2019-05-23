c5a8119642731a788ffdea7728850720584bd368
sinamoraddar <sinamoraddar@gmail.com>
2019-05-24 01:35:34 +0430

    initial commit

---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import react from 'react'
+class Test extends react.Component{
+    render(){
+        return(
+            <div>
+                hi there
+            </div>
+        );
+    }
+}
+export default Test
